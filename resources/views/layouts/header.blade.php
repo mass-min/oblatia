36ff51a908e2551d0a33cef9d69ff1669fa06bfc
mass-min <koyod111@gmail.com>
2019-02-16 23:53:06 +0900

    add heroku file

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,5 @@
 <header>
-    <img src="{{ asset('/img/logo_text.png') }}" style="width: 270px;" />
+    <a href="{{ route('index') }}">
+        <img src="{{ asset('/img/logo_text.png') }}" style="width: 270px;" />
+    </a>
 </header>
