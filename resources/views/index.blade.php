2fa9d5c2fa5a8028038700dfd2cf1423dcfb6657
mass-min <koyod111@gmail.com>
2019-02-17 00:16:27 +0900

    add favicon and font

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,15 +2,13 @@
 
 @section('content')
     <div class="container">
-        <h2 class="mt-3 mb-4 text-center">そのまま言わなくても、
-            <wbr>
-            いいんだよ。
+        <h2 class="mt-3 mb-4 text-center" style="font-family: 'Kosugi Maru', sans-serif;">そのまま言わなくても、<wbr>いいんだよ。
         </h2>
         {!! Form::open(['url' => route('translate'), 'method' => 'post']) !!}
         <div class="form-group">
             <textarea id="translation" name="translation" class="form-control mb-3"
                       placeholder="言いたいけど言えない本音を入力してください"></textarea>
-            <button type="submit" class="btn btn-primary">本音をオブラートに包む</button>
+            <button type="submit" class="btn btn-success">本音をオブラートに包む</button>
             {!! Form::close() !!}
         </div>
 @endsection
--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,1 +1,2 @@
+<link href="https://fonts.googleapis.com/css?family=Kosugi+Maru" rel="stylesheet">
 <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
